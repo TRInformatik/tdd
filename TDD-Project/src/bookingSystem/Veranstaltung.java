8042b77d0693e98c025fc665be5c80866d572475
Torben Rauhut <torben.rauhut@stud.hs-hannover.de>
2016-12-20 14:05:33 +0100

    Testen der Grundfunktiion der Vernastaltung

--- TDD-Project/src/bookingSystem/Veranstaltung.java
+++ TDD-Project/src/bookingSystem/Veranstaltung.java
@@ -2,9 +2,26 @@ package bookingSystem;
 
 public class Veranstaltung {
 
-	public String title() {
-		// TODO Auto-generated method stub
-		return "title";
+	private String title;
+	private int seats;
+	private double ticketPrice;
+	
+	public Veranstaltung(String title, double d, int i) {
+		this.title = title;
+		this.ticketPrice = d;
+		this.seats = i;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Object getSeats() {
+		return seats;
+	}
+
+	public Object getTicketPrice() {
+		return ticketPrice;
 	}
 
 }
--- TDD-Project/src/test/TestVeranstaltung.java
+++ TDD-Project/src/test/TestVeranstaltung.java
@@ -10,8 +10,14 @@ public class TestVeranstaltung {
 
 	@Test
 	public void test() {
-		Veranstaltung v = new Veranstaltung();
-		System.out.println(v.title());
+		Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", 13.0, 500);
+		Veranstaltung v2 = new Veranstaltung("Zytanien", 25.0, 1000);
+		assertEquals("Großes Fest im kleinen Garten", v.getTitle());
+		assertEquals(500, v.getSeats());
+		assertEquals(13.0, v.getTicketPrice());
+		assertEquals("Zytanien", v2.getTitle());
+		assertEquals(1000, v2.getSeats());
+		assertEquals(25.0, v2.getTicketPrice());
 	}
 
 }
