b587030fda9bfc8db2c1f69f8522f59cc71f69a0
Christian Hiber <christian.hiber@stud.hs-hannover.de>
2016-12-20 14:05:59 +0100

    Package kunde und Class Kunde hinzugefügt

---
+++ TDD-Project/src/kunde/Kunde.java
@@ -0,0 +1,38 @@
+package kunde;
+
+public class Kunde {
+
+	private String firstName, lastName;
+
+	public Kunde(String firstName) {
+		setFirstName(firstName);
+	}
+
+	public Kunde() {
+	}
+
+	public Kunde(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+}
---
+++ TDD-Project/src/test/TestKunde.java
@@ -0,0 +1,41 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import kunde.Kunde;
+
+public class TestKunde {
+
+	@Test
+	public void test() {
+		Kunde k;
+		k = new Kunde();
+		k.setFirstName("Christiane");
+		k.setLastName("Macke");
+		
+		String firstName = k.getFirstName();
+		assertEquals("Christiane", firstName);		
+		
+		String lastName = k.getLastName();
+		assertEquals("Macke", lastName);
+		
+		
+		
+		k = new Kunde("Hermann");
+		firstName = k.getFirstName();
+		assertEquals("Hermann", firstName);
+		
+		k = new Kunde("Christian", "Vogel");
+		firstName = k.getFirstName();
+		lastName = k.getLastName();
+		assertEquals("Christian", firstName);
+		assertEquals("Vogel", lastName);
+		
+		
+		
+		
+	}
+
+}
