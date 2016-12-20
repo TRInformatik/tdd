19ad52a106371f30eae11d371f65f5a4b0f24695
Torben Rauhut <torben.rauhut@stud.hs-hannover.de>
2016-12-20 15:12:25 +0100

    Veranstaltungs-Test 2.0!

--- TDD-Project/src/bookingSystem/Veranstaltung.java
+++ TDD-Project/src/bookingSystem/Veranstaltung.java
@@ -1,27 +1,49 @@
 package bookingSystem;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Veranstaltung {
 
 	private String title;
 	private int seats;
+	private int freeSeats;
 	private double ticketPrice;
+	DateFormat dfmt = new SimpleDateFormat( "dd.MM.yyyy HH:mm" );
+	private Date dateTime;
 	
-	public Veranstaltung(String title, double d, int i) {
+	public Veranstaltung(String title, String dateTime, double ticketPrice, int seats) throws ParseException {
 		this.title = title;
-		this.ticketPrice = d;
-		this.seats = i;
+		this.dateTime = dfmt.parse(dateTime);
+		this.ticketPrice = ticketPrice;
+		this.seats = seats;
+		this.freeSeats = this.seats;
 	}
 
 	public String getTitle() {
 		return title;
 	}
 
-	public Object getSeats() {
+	public int getSeats() {
 		return seats;
 	}
 
-	public Object getTicketPrice() {
+	public double getTicketPrice() {
 		return ticketPrice;
+	}
+
+	public String getDateTime() {
+		return dfmt.format(dateTime);
+	}
+
+	public void book(int bookedSeats) {
+		this.freeSeats -=bookedSeats;
+		
+	}
+	public int getFreeSeats() {
+		return freeSeats;
 	}
 
 }
--- TDD-Project/src/test/TestVeranstaltung.java
+++ TDD-Project/src/test/TestVeranstaltung.java
@@ -2,22 +2,48 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.text.ParseException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import bookingSystem.Veranstaltung;
 
 public class TestVeranstaltung {
-
+	
+	private static final double DELTA = 1e-15;
+		Veranstaltung v;
+		Veranstaltung v2; 
+	@Before 
+	public void init() throws ParseException {
+		 v = new Veranstaltung("Großes Fest im kleinen Garten", "01.01.2017 20:15", 13.0, 500);
+		 v2 = new Veranstaltung("Zytanien", "28.07.2017 16:00", 25.0, 1000);
+	}
 	@Test
-	public void test() {
-		Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", 13.0, 500);
-		Veranstaltung v2 = new Veranstaltung("Zytanien", 25.0, 1000);
+	public void testName() throws ParseException {
 		assertEquals("Großes Fest im kleinen Garten", v.getTitle());
-		assertEquals(500, v.getSeats());
-		assertEquals(13.0, v.getTicketPrice());
 		assertEquals("Zytanien", v2.getTitle());
+	}
+	
+	@Test
+	public void testSeatNumber() throws ParseException {
+		assertEquals(500, v.getSeats());
 		assertEquals(1000, v2.getSeats());
-		assertEquals(25.0, v2.getTicketPrice());
+	}
+	
+	@Test
+	public void testPrice() throws ParseException {
+		assertEquals(13.0, v.getTicketPrice(), DELTA);
+		assertEquals(25.0, v2.getTicketPrice(), DELTA);
+	}
+	
+	@Test
+	public void testBooking(){
+		v.book(5);
+		v.book(13);
+		v.book(2);
+		assertEquals(480, v.getFreeSeats());
+		assertEquals(500, v.getSeats());
 	}
 
 }
