18a8f10f15a347fa878c6232f44a6dd49aa0da22
Christian Hiber <christian.hiber@stud.hs-hannover.de>
2016-12-21 13:55:46 +0100

    DataManager now priceless

--- TDD-Project/src/bookingSystem/DataManager.java
+++ TDD-Project/src/bookingSystem/DataManager.java
@@ -9,12 +9,12 @@ import java.util.HashMap;
 
 public class DataManager {
 	private static final String FILE_PATH = "src/DataFiles/";
-	private static final String CUSTOMER_LIST_NAME = "Kunden";	
+	private static final String CUSTOMER_LIST_NAME = "Kunden";
 	private static final String EVENT_LIST_NAME = "Veranstaltung";
 	private static final String BOOKING_LIST_NAME = "Buchung";
-	
-	public DataManager(){
 
+	public DataManager() {
+		getCustomerList();//
 	}
 
 	@SuppressWarnings("unchecked")
@@ -22,35 +22,40 @@ public class DataManager {
 		HashMap<String, Kunde> customerList = null;
 		FileInputStream fileInputStream = null;
 		ObjectInputStream objectInputStream = null;
+
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-		fileInputStream = new FileInputStream(pathToLists+"/"+CUSTOMER_LIST_NAME);
-		objectInputStream = new ObjectInputStream(fileInputStream);
-		customerList = (HashMap<String, Kunde>) objectInputStream.readObject();
-		objectInputStream.close();
-		fileInputStream.close();
-		
-		}catch(Exception e){
+			// check if file exists
+			if (!fileExists(CUSTOMER_LIST_NAME)) {
+				createNewFile(CUSTOMER_LIST_NAME);
+			}
+			fileInputStream = new FileInputStream(pathToLists + "/" + CUSTOMER_LIST_NAME);
+			objectInputStream = new ObjectInputStream(fileInputStream);
+			customerList = (HashMap<String, Kunde>) objectInputStream.readObject();
+			objectInputStream.close();
+			fileInputStream.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 		return customerList;
 	}
-	
-	public void storeCustomerList(HashMap<String, Kunde> customerList){
+
+	public void storeCustomerList(HashMap<String, Kunde> customerList) {
 		FileOutputStream fileOutputStream = null;
 		ObjectOutputStream objectOutputStream = null;
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-			fileOutputStream = new FileOutputStream(pathToLists+"/"+CUSTOMER_LIST_NAME);
+			fileOutputStream = new FileOutputStream(pathToLists + "/" + CUSTOMER_LIST_NAME);
 			objectOutputStream = new ObjectOutputStream(fileOutputStream);
-			objectOutputStream.writeObject(customerList);			
+			objectOutputStream.writeObject(customerList);
 			objectOutputStream.close();
-			fileOutputStream.close();	
-		}catch(Exception e){
+			fileOutputStream.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public HashMap<Integer, Veranstaltung> getEventList() {
 		HashMap<Integer, Veranstaltung> eventList = null;
@@ -58,33 +63,37 @@ public class DataManager {
 		ObjectInputStream objectInputStream = null;
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-		fileInputStream = new FileInputStream(pathToLists+"/"+EVENT_LIST_NAME);
-		objectInputStream = new ObjectInputStream(fileInputStream);
-		eventList = (HashMap<Integer, Veranstaltung>) objectInputStream.readObject();
-		objectInputStream.close();
-		fileInputStream.close();
-		
-		}catch(Exception e){
+			// check if file exists
+			if (!fileExists(EVENT_LIST_NAME)) {
+				createNewFile(EVENT_LIST_NAME);
+			}
+			fileInputStream = new FileInputStream(pathToLists + "/" + EVENT_LIST_NAME);
+			objectInputStream = new ObjectInputStream(fileInputStream);
+			eventList = (HashMap<Integer, Veranstaltung>) objectInputStream.readObject();
+			objectInputStream.close();
+			fileInputStream.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 		return eventList;
 	}
-	
-	public void storeEventList(HashMap<Integer, Veranstaltung> eventList){
+
+	public void storeEventList(HashMap<Integer, Veranstaltung> eventList) {
 		FileOutputStream fileOutputStream = null;
 		ObjectOutputStream objectOutputStream = null;
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-			fileOutputStream = new FileOutputStream(pathToLists+"/"+EVENT_LIST_NAME);
+			fileOutputStream = new FileOutputStream(pathToLists + "/" + EVENT_LIST_NAME);
 			objectOutputStream = new ObjectOutputStream(fileOutputStream);
-			objectOutputStream.writeObject(eventList);			
+			objectOutputStream.writeObject(eventList);
 			objectOutputStream.close();
-			fileOutputStream.close();	
-		}catch(Exception e){
+			fileOutputStream.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public HashMap<Integer, Buchung> getBookingList() {
 		HashMap<Integer, Buchung> bookingList = null;
@@ -92,15 +101,19 @@ public class DataManager {
 		ObjectInputStream objectInputStream = null;
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-		fileInputStream = new FileInputStream(pathToLists+"/"+BOOKING_LIST_NAME);
-		objectInputStream = new ObjectInputStream(fileInputStream);
-		bookingList = (HashMap<Integer, Buchung>) objectInputStream.readObject();
-		objectInputStream.close();
-		fileInputStream.close();
-		
-		}catch(Exception e){
+			// check if file exists
+			if (!fileExists(BOOKING_LIST_NAME)) {
+				createNewFile(BOOKING_LIST_NAME);
+			}
+			fileInputStream = new FileInputStream(pathToLists + "/" + BOOKING_LIST_NAME);
+			objectInputStream = new ObjectInputStream(fileInputStream);
+			bookingList = (HashMap<Integer, Buchung>) objectInputStream.readObject();
+			objectInputStream.close();
+			fileInputStream.close();
+
+		} catch (Exception e) {
 			e.printStackTrace();
-		}	
+		}
 		return bookingList;
 	}
 
@@ -109,14 +122,39 @@ public class DataManager {
 		ObjectOutputStream objectOutputStream = null;
 		try {
 			String pathToLists = new File(FILE_PATH).getCanonicalPath().toString();
-			fileOutputStream = new FileOutputStream(pathToLists+"/"+BOOKING_LIST_NAME);
+			fileOutputStream = new FileOutputStream(pathToLists + "/" + BOOKING_LIST_NAME);
 			objectOutputStream = new ObjectOutputStream(fileOutputStream);
-			objectOutputStream.writeObject(bookingList);			
+			objectOutputStream.writeObject(bookingList);
 			objectOutputStream.close();
-			fileOutputStream.close();	
-		}catch(Exception e){
+			fileOutputStream.close();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
+	}
+
+	private boolean fileExists(String filename) {
+		for (File fileEntry : new File(FILE_PATH).listFiles()) {
+			if (!fileEntry.isDirectory()) {
+				if (fileEntry.getName().equals(filename)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	private void createNewFile(String filename) {
+		FileOutputStream fileOutputStream = null;
+		ObjectOutputStream objectOutputStream = null;
+		try {
+			fileOutputStream = new FileOutputStream(FILE_PATH + "/" + filename);
+			objectOutputStream = new ObjectOutputStream(fileOutputStream);
+			objectOutputStream.writeObject(null);
+			objectOutputStream.close();
+			fileOutputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- TDD-Project/src/test/TestDataManager.java
+++ TDD-Project/src/test/TestDataManager.java
@@ -14,115 +14,104 @@ import bookingSystem.Kunde;
 import bookingSystem.Veranstaltung;
 
 public class TestDataManager {
-	
+
 	public DataManager dataManager;
-	
-	@Before 
-	public void init(){
+
+	@Before
+	public void init() {
 		dataManager = new DataManager();
 	}
-	
+
 	@Test
 	public void testCustomer() {
-		
-		
+
 		// The customer to store
 		String name = "Hans Wurst";
 		String address = "Straße 1, PLZ1, Ort";
-		
+
 		HashMap<String, Kunde> customerList = new HashMap<String, Kunde>();
 		customerList.put(name, new Kunde(name, address));
 		dataManager.storeCustomerList(customerList);
-		
+
 		name = "Christiane Macke";
 		address = "Neue Straße1, 4711, Köln";
 		customerList.put(name, new Kunde(name, address));
 		dataManager.storeCustomerList(customerList);
 		assertEquals(2, customerList.size());
-		
-		
+
 		Kunde hans = customerList.get("Hans Wurst");
 		assertEquals(hans.getClass(), Kunde.class);
 		assertEquals("Hans Wurst", hans.getName());
 		assertEquals("Straße 1, PLZ1, Ort", hans.getAddress());
-		
-		
+
 		Kunde christiane = customerList.get("Christiane Macke");
 		assertEquals(hans.getClass(), Kunde.class);
 		assertEquals("Christiane Macke", christiane.getName());
 		assertEquals("Neue Straße1, 4711, Köln", christiane.getAddress());
-		
+
 	}
-	
+
 	@Test
 	public void testEvent() throws ParseException {
 
 		// The customer to store
-		 Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", "01.01.2017 20:15", 13.0, 500);
-		 Veranstaltung v2 = new Veranstaltung("Zytanien", "28.07.2017 16:00", 25.0, 1000);
-		 Veranstaltung v3 = new Veranstaltung("Geburtstag", "05.06.2017 18:00", 0.0, 20);
-		
+		Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", "01.01.2017 20:15", 13.0, 500);
+		Veranstaltung v2 = new Veranstaltung("Zytanien", "28.07.2017 16:00", 25.0, 1000);
+		Veranstaltung v3 = new Veranstaltung("Geburtstag", "05.06.2017 18:00", 0.0, 20);
+
 		HashMap<Integer, Veranstaltung> eventList = new HashMap<Integer, Veranstaltung>();
 		eventList.put(v.getID(), v);
 		dataManager.storeEventList(eventList);
-		
+
 		eventList.put(v2.getID(), v2);
 		dataManager.storeEventList(eventList);
 		assertEquals(2, eventList.size());
-		
-		
+
 		Veranstaltung vt1 = eventList.get(v.getID());
 		assertEquals(vt1.getClass(), Veranstaltung.class);
 		assertEquals(v.getTitle(), vt1.getTitle());
 		assertEquals(v.getDateTime(), vt1.getDateTime());
 		assertEquals(v.getSeats(), vt1.getSeats());
-		
-		
+
 		Veranstaltung vt2 = eventList.get(v2.getID());
 		assertEquals(vt2.getClass(), Veranstaltung.class);
 		assertEquals(v2.getTitle(), vt2.getTitle());
 		assertEquals(v2.getDateTime(), vt2.getDateTime());
 		assertEquals(v2.getSeats(), vt2.getSeats());
-		
-		
+
 	}
-	
+
 	@Test
 	public void testBooking() throws ParseException {
-
 		// The customer to store
-		 Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", "01.01.2017 20:15", 13.0, 500);
-		 Veranstaltung v2 = new Veranstaltung("Zytanien", "28.07.2017 16:00", 25.0, 1000);
-		 Veranstaltung v3 = new Veranstaltung("Geburtstag", "05.06.2017 18:00", 0.0, 20);
-		 String name = "Hans Wurst";
+		Veranstaltung v = new Veranstaltung("Großes Fest im kleinen Garten", "01.01.2017 20:15", 13.0, 500);
+		Veranstaltung v2 = new Veranstaltung("Zytanien", "28.07.2017 16:00", 25.0, 1000);
+		Veranstaltung v3 = new Veranstaltung("Geburtstag", "05.06.2017 18:00", 0.0, 20);
+		String name = "Hans Wurst";
 		String lastName = "Wurst";
 		String address = "Straße 1, PLZ1, Ort";
-		Kunde k1 =  new Kunde(name, address);
+		Kunde k1 = new Kunde(name, address);
 		Buchung b1 = new Buchung(k1.getName(), v3.getID(), 3);
-		
+
 		HashMap<Integer, Buchung> bookingList = new HashMap<Integer, Buchung>();
 		bookingList.put(b1.getID(), b1);
 		dataManager.storeBookingList(bookingList);
-		
+
 		Buchung b2 = new Buchung(k1.getName(), v2.getID(), 4);
 		bookingList.put(b2.getID(), b2);
 		dataManager.storeBookingList(bookingList);
 		assertEquals(2, bookingList.size());
-		
-		
+
 		Buchung bt1 = bookingList.get(b1.getID());
 		assertEquals(bt1.getClass(), Buchung.class);
 		assertEquals(v3.getID(), bt1.getVeranstaltung());
 		assertEquals(k1.getName(), bt1.getKunde());
 		assertEquals(b1.getBookedSeats(), bt1.getBookedSeats());
-		
-		
+
 		Buchung bt2 = bookingList.get(b2.getID());
 		assertEquals(bt2.getClass(), Buchung.class);
 		assertEquals(v2.getID(), bt2.getVeranstaltung());
 		assertEquals(k1.getName(), bt2.getKunde());
 		assertEquals(b2.getBookedSeats(), bt2.getBookedSeats());
-		
-		
 	}
 }
